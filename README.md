98455bf30264f80f99d19d15a02d659ddd9c9e2f
ElCeqo <fr.corvaglia@gmail.com>
2024-07-02 20:21:10 +0200

    Changed init configuration for FSK mode

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Zefirus-Software
+
+### Commsys
+commsys uses the FSK Modem, details in section 4.2 of the documentation
+
+Register map is at section 6.2
--- modules/CommSystem.py
+++ modules/CommSystem.py
@@ -8,12 +8,22 @@ This class will be responsible for the communication system of the CubeSat.
 The communication system will be responsible for sending and receiving data from the ground station.
 
 The OBC is connected via SPI to the radio module.
+
+
+device address: 0x33
+broadcast address: 0x44
+
+
+In order to retrieve received data from FIFO the user must ensure that ValidHeader, PayloadCrcError, RxDone and
+RxTimeout interrupts in the status register RegIrqFlags are not asserted to ensure that packet reception has terminated
+successfully (i.e. no flags should be set).
+
 '''
 
 
 class CommSystem:
     
-    def __init__(self, bus=0, device=0, irq_pin=25, reset_pin=17, device_address=0x00):
+    def __init__(self, bus=0, device=0, irq_pin=25, reset_pin=7, device_address=0x33, fifo_address=0x00):
         '''
         Initialize the communication system by connecting to the RFM95/96/97/98(W) 
         Low Power Long Range Transceiver Module.
@@ -21,14 +31,14 @@ class CommSystem:
         self.bus = bus
         self.device = device
         self.device_address = device_address
+        self.fifo_address = fifo_address
 
         # Set up SPI
         self.spi = spidev.SpiDev()
         self.spi.open(bus, device)
-        self.spi.max_speed_hz = 500000
 
         # Set up GPIO for IRQ and RESET
-        self.irq_pin = irq_pin
+        self.irq_pin = irq_pin            # still to be defined
         self.reset_pin = reset_pin
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.irq_pin, GPIO.IN)
@@ -38,7 +48,7 @@ class CommSystem:
         self.reset()
         
         # Initialize the LoRa module
-        self.init_lora()
+        self.init_FSK()
 
     def reset(self):
         '''
@@ -49,16 +59,24 @@ class CommSystem:
         GPIO.output(self.reset_pin, GPIO.HIGH)
         time.sleep(0.01)
 
-    def init_lora(self):
+    def init_FSK(self):
         '''
         Initialize the LoRa module with the desired settings.
         '''
-        # Basic configuration example
-        self.write_register(0x01, 0x80) # Long range mode (LoRa mode)
+        # Basic configuration for FSK mode
+        # first set the module to sleep mode
+        self.RegOpMode = 0x01
+        self.write_register(self.RegOpMode, 0x00)
+        # set the module to FSK mode, can be done only after the module is in sleep mode
+        self.write_register(0x01, 0x00)
+
+        # Carrier frequency registers setup (0x06, 0x07, 0x08)
         self.write_register(0x06, 0x6C) # Frequency (example: 915 MHz)
         self.write_register(0x07, 0x80)
         self.write_register(0x08, 0x00)
-        self.write_register(0x09, 0xF8) # Max power
+
+        # Power Amplifier setup (0x09)
+        self.write_register(0x09, 0x7F) # Max power
 
     def write_register(self, address, value):
         '''
